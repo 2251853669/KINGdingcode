044a23028bf0b633301f0d4f3ef545e8516a06c5
872239809 <872239809@qq.com>
2025-01-07 15:42:23 +0800

    封装GETSAL类，创建Main类

--- dingding/GETSAL.java
+++ dingding/GETSAL.java
@@ -1,5 +1,5 @@
 package com.kingdee.webapi.javasdk.dingding;
-//从金蝶获取销售订单数据对比发货数量和实际发货数量生成图片，并发送钉钉
+
 import com.google.gson.*;
 import com.kingdee.bos.webapi.sdk.K3CloudApi;
 
@@ -8,11 +8,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Logger;
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 
 class DateUtils {
     public static String getStartDate() {
@@ -29,34 +26,26 @@ class DateUtils {
     }
 }
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GETSAL {
     private static final Logger LOGGER = Logger.getLogger(GETSAL.class.getName());
 
-    public static void main(String[] args) {
+    public static void execute() {
         K3CloudApi client = new K3CloudApi();
         String startDate = DateUtils.getStartDate().replaceAll("'", "\\'");
         String endDate = DateUtils.getEndDate().replaceAll("'", "\\'");
         System.out.println(startDate);
         System.out.println(endDate);
-        /*
-        FBILLNO 单据编号
-        FCREATEDATE  创建时间
-        FQTY  销售数量
-        FSUMOUTQTY 累计出库数量
-        * */
 
-        //定义接口参数 jsonData
         String FormId = "SAL_DELIVERYNOTICE";
         String FieldKeys = "FBILLNO,FCREATEDATE,FDOCUMENTSTATUS,FQTY,FSUMOUTQTY";
-        String FilterString = "FCREATEDATE between " +  startDate  + " and " + endDate + " and FDOCUMENTSTATUS = 'C' and FQTY <> FSUMOUTQTY";
+        String FilterString = "FCREATEDATE between " + startDate + " and " + endDate + " and FDOCUMENTSTATUS = 'C' and FQTY <> FSUMOUTQTY";
         String OrderString = "";
         int TopRowCount = 0;
         int StartRow = 0;
         int EndRow = 0;
         int Limit = 2000;
         String SubSystemId = "";
-        // 构造jsonData
+
         String jsonData = "{\"FormId\":\"" + FormId + "\"," +
                 "\"FieldKeys\":\"" + FieldKeys + "\"," +
                 "\"FilterString\":\"" + FilterString + "\"," +
@@ -66,18 +55,19 @@ public class GETSAL {
                 "\"EndRow\":" + EndRow + "," +
                 "\"Limit\":" + Limit + "," +
                 "\"SubSystemId\":\"" + SubSystemId + "\"}";
+/*
         System.out.println("接口发送数据: " + jsonData);
+        */
         try {
-            // 调用接口
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             String resultJson = gson.toJson(client.executeBillQuery(jsonData));
+            /*
             System.out.println("接口返回结果: " + resultJson);
+            */
             JsonParser parser = new JsonParser();
-            // 解析JSON
             JsonElement jsonElement = parser.parse(resultJson);
             JsonArray jsonArray = jsonElement.getAsJsonArray();
             List<String[]> data = new ArrayList<>();
-            // 遍历JSON数组 并将数据添加到List中
             for (JsonElement element : jsonArray) {
                 JsonArray innerArray = element.getAsJsonArray();
                 int arraySize = innerArray.size();
@@ -88,21 +78,13 @@ public class GETSAL {
                 data.add(row);
             }
             TableToImage.createTableImage(data, "table.png");
-            //输出文件路径
-
             System.out.println("生成图片的路径: " + new File(".").getAbsolutePath() + File.separator + "table.png");
 
-
-                String urlString = "https://api.xinyew.cn/api/360tc";
-                String filePath = "D:\\Senddingcode\\KINGdingcode\\table.png";
-
-                DingTalkNotification.sendDingTalkNotification(urlString, filePath);
-
+            String urlString = "https://api.xinyew.cn/api/360tc";
+            String filePath = "D:\\Senddingcode\\KINGdingcode\\table.png";
+            DingTalkNotification.sendDingTalkNotification(urlString, filePath);
         } catch (Exception e) {
             LOGGER.severe(e.getMessage());
-
         }
     }
-
-
 }
---
+++ dingding/Main.java
@@ -0,0 +1,7 @@
+package com.kingdee.webapi.javasdk.dingding;
+
+public class Main {
+    public static void main(String[] args) {
+        GETSAL.execute();
+    }
+}
